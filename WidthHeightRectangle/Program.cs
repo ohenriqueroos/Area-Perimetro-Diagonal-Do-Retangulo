1ea42d137c93858583e8ecda1f2367ef95d00aea
henri <henri@HENRIQUE>
2024-04-23 22:54:03 -0300

    Add project files.

---
+++ WidthHeightRectangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace WidthHeightRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Informe a largura e altura do retângulo: ");
+            Console.Write("Largura: ");
+            rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("ÁREA = " + rectangle.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + rectangle.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + rectangle.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ WidthHeightRectangle/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿namespace WidthHeightRectangle
+{
+    class Rectangle
+    {
+        public double Width;
+        public double Height;
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return 2.0 * (Width + Height);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Height * Height) + (Width * Width));            
+        }
+    }
+}
